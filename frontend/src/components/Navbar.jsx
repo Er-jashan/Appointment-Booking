7abed494e467eb8a80b4090b181833a2b5c0e0d2
Harmanpreet Singh <harmanbajwa012005@gmail.com>
2025-08-18 23:50:42 +0530

    Logo updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [token , setToken] = useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img onClick={()=>{navigate('/')}} className='w44 cursor-pointer' src={assets.logo}/>
+      <img onClick={()=>{navigate('/')}} className='w-48 cursor-pointer' src={assets.logo}/>
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
           <li className='py-1'>Home</li>
