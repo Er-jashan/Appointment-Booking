6b50d0932b81307c47e55e064ee6501836c5c399
Harmanpreet Singh <harmanbajwa012005@gmail.com>
2025-08-19 00:03:38 +0530

    git commit

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -38,10 +38,10 @@ const Doctors = () => {
           <p onClick={()=> speciality === 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gastroenterologist" ? "bg-indigo-100 text-black" : ""}`}>Gastroenterologist</p>
         </div>
 
-        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6 '>
           {
             filterDoc.map((item , index)=>(
-           <div onClick={()=> navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden hover:translate-y-[-10px] transition-all duration-500' key={index}>
+           <div onClick={()=> navigate(`/appointment/${item._id}`)} className='border cursor-pointer border-blue-200 rounded-xl overflow-hidden hover:translate-y-[-10px] hover:bg-gray-300 transition-all duration-300 hover:shadow-lg' key={index}>
             <img className='bg-blue-50 ' src={item.image} alt=''/>
             <div className='p-4'>
                 <div className='flex items-center gap-2 text-sm text-center text-green-500 '>
