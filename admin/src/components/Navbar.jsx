c589e0d15cb464ff99316987f8a4c3c09956b41d
Harmanpreet Singh <harmanbajwa012005@gmail.com>
2025-08-22 19:29:44 +0530

    Add-doctors-dashboard

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       navigate('/')
     }
   return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b border-gray-300 bg-white'>
       <div className='flex items-center gap-2 text-xs'>
         <img className='w-40 sm:w-48 cursor-pointer' src={assets.admin_logo} alt="Medicura-logo" />
         <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
