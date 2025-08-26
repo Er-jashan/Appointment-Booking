4f24c11b5c7cf4e1513fe1dfb65552fefbe98b8f
Harmanpreet Singh <harmanbajwa012005@gmail.com>
2025-08-26 10:24:52 +0530

    minor changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
-import appointmentModel from '../models/AppointmentModel.jsx'
+import appointmentModel from '../models/AppointmentModel.js'
 
 
 //api to register user
